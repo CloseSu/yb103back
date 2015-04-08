f7ff43ff74462ef9697e151c380e6434003be3b7
CloseSu <wei33107@gmail.com>
2015-04-08 19:03:12 +0800

    20150408

--- Backup/R/REvised.R
+++ Backup/R/REvised.R
@@ -3,8 +3,11 @@ banana = read.xlsx(header=TRUE,file='Taichung_banana.xlsx',sheetIndex=1,encoding
 
 head(banana[4:624,2])
 
-revise = banana[4:624,2]
+str(banana)
 
+revise = banana[4:623,2]
+
+write.csv(revise,file='ttt.csv')
 
 Revised2 = data.frame(revise,revise,revise,revise,revise,revise,revise,revise,revise,revise)
 
@@ -12,23 +15,19 @@ write.csv(Revised2,file='text.csv')
 
 head(Revised2)
 
-lastweek = seq(3,624,3)
+lastweek = seq(3,623,3)
+
+Revised2[624,]
 
 lastmonth = Revised2[lastweek,]
+
+
+write.csv(lastmonth,file='text2.csv')
 
 average = lastweek/3
 
 Revised2[1,11]
 
-
-##===================
-if (average%12 ==1 |average%12 ==3 |average%12 ==5 |average%12 ==7|average%12 ==8 |average%12 ==10 |average%12 ==12 ){
-  Revised2[lastweek,11] = Revised2[lastweek,10]
-}else if(average%12 ==2){
-  Revised2[lastweek,9:10]=NULL
-}
-
-###=====================
 
 
 
@@ -39,6 +38,19 @@ write.csv(av,file='text.csv')
 
 
 str(av)
+
+
+RDateData = read.xlsx(header=TRUE,file='text23.xlsx',sheetIndex=1,encoding='UTF-8' )
+
+revised = RDateData[,-1]
+
+year = seq(1998,2015,1)
+
+month = seq(1,12,1)
+
+
+
+
 
 
 
